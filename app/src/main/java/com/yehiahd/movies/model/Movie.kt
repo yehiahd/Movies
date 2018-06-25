05bc072d48220d472b4fefd67b78f9f46a9b657a
Mohamed Yehia <yehia_hd@yahoo.com>
2018-06-25 04:25:19 +0200

    Add Parclize Annotation

--- app/src/main/java/com/yehiahd/movies/model/Movie.kt
+++ app/src/main/java/com/yehiahd/movies/model/Movie.kt
@@ -1,12 +1,15 @@
 package com.yehiahd.movies.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 data class MoviesResponse(@SerializedName("results") val movies: List<Movie>,
                           @SerializedName("page") val page: Int,
                           @SerializedName("total_pages") val totalPages: Int,
                           @SerializedName("total_results") val totalResults: Int)
 
+@Parcelize
 data class Movie(@SerializedName("vote_average") val voteAverage: Double,
                  @SerializedName("backdrop_path") var backdropPath: String,
                  @SerializedName("id") val id: Int,
@@ -17,4 +20,4 @@ data class Movie(@SerializedName("vote_average") val voteAverage: Double,
                  @SerializedName("release_date") val releaseDate: String,
                  @SerializedName("original_title") val originalTitle: String,
                  @SerializedName("original_language") val originalLanguage: String,
-                 @SerializedName("vote_count") val voteCount: Int)
+                 @SerializedName("vote_count") val voteCount: Int) : Parcelable
