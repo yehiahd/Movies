8a8eb31fd8f51ec1b7a1e6e84d4df9c8334fb293
Mohamed Yehia <yehia_hd@yahoo.com>
2018-06-27 08:06:23 +0200

    Inject dependencies into details ViewModel

--- app/src/main/java/com/yehiahd/movies/ui/detailscreen/DetailViewModel.kt
+++ app/src/main/java/com/yehiahd/movies/ui/detailscreen/DetailViewModel.kt
@@ -1,6 +1,8 @@
 package com.yehiahd.movies.ui.detailscreen
 
+import com.yehiahd.movies.datamodel.DataManager
 import com.yehiahd.movies.ui.base.BaseViewModel
+import javax.inject.Inject
 
-class DetailViewModel : BaseViewModel() {
+class DetailViewModel @Inject constructor(private val dataManager: DataManager) : BaseViewModel() {
 }
