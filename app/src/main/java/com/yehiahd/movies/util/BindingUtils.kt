eb00d55229619346bf4a1e8f864307e0d97c93d0
Mohamed Yehia <yehia_hd@yahoo.com>
2018-06-29 20:34:02 +0200

    Add Images PlaceHolder/Error

--- app/src/main/java/com/yehiahd/movies/util/BindingUtils.kt
+++ app/src/main/java/com/yehiahd/movies/util/BindingUtils.kt
@@ -3,11 +3,14 @@ package com.yehiahd.movies.util
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import com.yehiahd.movies.R
 
 
 @BindingAdapter(value = ["android:loadImage"])
 fun loadImage(view: ImageView, imgUrl: String) {
     Picasso.get()
             .load(imgUrl)
+            .placeholder(R.drawable.imgs_placeholder)
+            .error(R.drawable.imgs_error)
             .into(view)
 }
