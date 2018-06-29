7c7869b5375d1f0825e25f646e0d452d954d4fdf
Mohamed Yehia <yehia_hd@yahoo.com>
2018-06-29 21:10:54 +0200

    Add Some Details

--- app/src/main/java/com/yehiahd/movies/ui/detailscreen/DetailScreen.kt
+++ app/src/main/java/com/yehiahd/movies/ui/detailscreen/DetailScreen.kt
@@ -3,7 +3,7 @@ package com.yehiahd.movies.ui.detailscreen
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.yehiahd.movies.R
-import com.yehiahd.movies.databinding.ActivityDetailScreenBinding
+import com.yehiahd.movies.databinding.ActivityDetailBinding
 import com.yehiahd.movies.ui.base.BaseActivity
 import com.yehiahd.movies.util.Constant
 
@@ -12,7 +12,7 @@ class DetailScreen : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityDetailScreenBinding>(this, R.layout.activity_detail_screen)
+        val binding = DataBindingUtil.setContentView<ActivityDetailBinding>(this, R.layout.activity_detail)
         binding.movie = intent.extras.getParcelable(Constant.Extra.MOVIE)
     }
 
